84f51b11bcd61433639703d9dd78d4f7d13897f8
kslim76 <limkokseng@hotmail.com>
2023-03-21 08:20:16 +0530

    Catalog modified y user3

--- CatalogAPI/Models/Catalog.cs
+++ CatalogAPI/Models/Catalog.cs
@@ -19,6 +19,8 @@ namespace CatalogAPI.Models
         [Column("Catalog_Name")]
         [Required]
         [StringLength(50)]
+	//user3 modifying the code
+	//he wanat to additional constaint to catalog name as well
         public string? CatalogName { get; set; }
 
         //[JsonIgnore]       
